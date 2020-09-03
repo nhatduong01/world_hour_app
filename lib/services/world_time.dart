7cf283f113f01bca6c07194d2250e840dca2381c
DuongNguyen <nhatduong30001@gmail.com>
2020-09-03 12:23:56 +0700

    I delete some stuffs

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -17,12 +17,9 @@ class GioTheGioi
       //Make the request
       Response response = await get('http://worldtimeapi.org/api/timezone/$url');
       Map data = jsonDecode(response.body);
-      //print(data);
       // Get properties from the data
       String datetime = data['datetime'];
       String offset = data['utc_offset'].substring(1,3);
-      //print(datetime);
-      // print(offset);
       DateTime now = DateTime.parse(datetime);
       now = now.add(Duration(hours: int.parse(offset)));
       // Get the time properties
